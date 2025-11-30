0edf446076b5377d6e27ce3aa229f6925c425241
makeworking <80930713+bboymak3@users.noreply.github.com>
2025-11-30 02:42:42 -0400

    gg
    gg

--- functions/api/ordenes.js
+++ functions/api/ordenes.js
@@ -8,7 +8,6 @@
         cliente_nombre as nombre,           -- ← MAPEAR aquí
         cliente_telefono as telefono,       -- ← MAPEAR aquí  
         cliente_email as email,             -- ← MAPEAR aquí
-	ticket_id as tickets,           -- ← MOSTRAR los tickets aquí
         total, 
         metodo_pago, 
         comprobante, 
