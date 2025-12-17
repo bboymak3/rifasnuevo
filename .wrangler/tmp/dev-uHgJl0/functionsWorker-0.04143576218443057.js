3dd15d450b7c54ba12bc3a1123e806dfd94e6d52
makeworking <80930713+bboymak3@users.noreply.github.com>
2025-12-17 00:16:41 -0400

    gg
    gg

--- .wrangler/tmp/dev-uHgJl0/functionsWorker-0.04143576218443057.js
+++ .wrangler/tmp/dev-uHgJl0/functionsWorker-0.04143576218443057.js
@@ -1,7 +1,7 @@
 var __defProp = Object.defineProperty;
 var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
 
-// .wrangler/tmp/bundle-Qy9f0L/checked-fetch.js
+// .wrangler/tmp/bundle-TF4X3Q/checked-fetch.js
 var urls = /* @__PURE__ */ new Set();
 function checkURL(request, init) {
   const url = request instanceof URL ? request : new URL(
@@ -1167,7 +1167,7 @@ var jsonError2 = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx
 }, "jsonError");
 var middleware_miniflare3_json_error_default2 = jsonError2;
 
-// .wrangler/tmp/bundle-Qy9f0L/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-TF4X3Q/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__2 = [
   middleware_ensure_req_body_drained_default2,
   middleware_miniflare3_json_error_default2
@@ -1199,7 +1199,7 @@ function __facade_invoke__2(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__2, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-Qy9f0L/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-TF4X3Q/middleware-loader.entry.ts
 var __Facade_ScheduledController__2 = class ___Facade_ScheduledController__2 {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
